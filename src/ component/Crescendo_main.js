0a3031471efe21c0b90e6c1bdde6ba76b66884b1
다빈 <prospecs1039@daum.net>
2024-01-15 20:52:29 +0900

    리액트 초기세팅

---
+++ src/ component/Crescendo_main.js
@@ -0,0 +1,16 @@
+
+import React, {useState} from "react";
+import { MdAdd, MdDelete, MdDone } from "react-icons/md";
+
+import './scss/Crescendo_main.scss';
+
+const Crescendo_main = () => {
+
+    return (
+        <div className='CrescendoMain'>
+            테스트합니다
+        </div>
+    );
+};
+
+export default Crescendo_main;
