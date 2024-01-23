e83a3d71a9560e11a2a6d508ffd3e5490662d24b
20230720 <ghkdwo34@naver.com>
2024-01-23 14:26:18 +0900

    검색 기능 추가(유튜브 링크1)

--- src/ component/UI/Page/Conversion/Conversion_Separation/ConversionSearch.js
+++ src/ component/UI/Page/Conversion/Conversion_Separation/ConversionSearch.js
@@ -1,33 +1,39 @@
 import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
+import axios from 'axios';
 import '././CoverdionSearch.scss';
 
 const ConversionSearch = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState([]);
 
-    const handleSearch = () => {
-        // 간단한 검색 결과 배열을 시뮬레이션합니다.
-        const simulatedSearchResults = [
-            { id: 1, title: '검색 결과 1', videoId: 'VIDEO_ID_1' },
-            { id: 2, title: '검색 결과 2', videoId: 'VIDEO_ID_2' },
-            { id: 3, title: '검색 결과 3', videoId: 'VIDEO_ID_3' },
-            { id: 4, title: '검색 결과 4', videoId: 'VIDEO_ID_4' },
-            { id: 5, title: '검색 결과 5', videoId: 'VIDEO_ID_5' },
-        ];
-        // 시뮬레이션된 검색 결과를 상태에 설정합니다.
-        setSearchResults(simulatedSearchResults);
+    const handleSearch = async () => {
+        try {
+            // 검색어가 비어 있으면 검색하지 않음
+            if (!searchTerm) return;
+
+            // 예시: 백엔드 엔드포인트 URL (적절한 주소로 변경해야 함)
+            // 너가 만든 localhost 적용
+            const backendUrl = 'http://localhost:5000/api/convert';
+
+            // 서버로 검색 요청 보냄
+            const response = await axios.post(backendUrl, { youtubeLink: searchTerm });
+
+            // 백엔드에서 받은 검색 결과를 상태에 설정
+            setSearchResults(response.data);
+        } catch (error) {
+            console.error('검색 중 에러 발생:', error);
+        }
 
         // 검색을 처리한 후 검색어 입력값 초기화
         setSearchTerm('');
     };
 
-    // 검색 결과 항목을 클릭할 때 유튜브 링크로 이동하는 함수
-    const handleResultClick = (videoId) => {
-        // 유튜브 동영상 링크로 이동
-        window.location.href = `https://www.youtube.com/watch?v=${videoId}`;
+    // 검색 결과 항목을 클릭할 때 악보 변환 페이지로 이동하는 함수
+    const handleResultClick = (conversionLink) => {
+        // 악보 변환 페이지로 이동
+        window.location.href = conversionLink;
     };
-
     return (
         <div className="w">
             <div className="search">
@@ -52,7 +58,7 @@ const ConversionSearch = () => {
                 <h3>검색 결과</h3>
                 <ul>
                     {searchResults.map((result) => (
-                        <li key={result.id} onClick={() => handleResultClick(result.videoId)}>
+                        <li key={result.id} onClick={() => handleResultClick(result.conversionLink)}>
                             <p>{result.title}</p>
                         </li>
                     ))}
@@ -61,5 +67,4 @@ const ConversionSearch = () => {
         </div>
     );
 };
-
 export default ConversionSearch;
