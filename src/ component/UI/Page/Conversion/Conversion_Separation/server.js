1c544ddc72cbe61151744e30b58227da5d9c495e
Won.Y.Jae <128454859+Y-ja@users.noreply.github.com>
2024-01-24 21:48:54 +0900

    Update server.js
    검색기능4

--- src/ component/UI/Page/Conversion/Conversion_Separation/server.js
+++ src/ component/UI/Page/Conversion/Conversion_Separation/server.js
@@ -1,6 +1,5 @@
 const LOCAL_PORT: string = '8484';
 const API_BASE_URL = 'http://localhost:';
-const SCORE : string = '/api/score/youtube ';
-const AUTH = '/api/auth';
+const SCORE: string = '/api/score/youtube ';
 export const SCORE_URL = API_BASE_URL + SCORE;
-export const AUTH_URL = API_BASE_URL + AUTH;
+// export const AUTH_URL = API_BASE_URL + '/api/auth'; // 이 부분은 아직 사용되지 않으므로 주석 처리
