4f85ec4f5bac9b03cb1aaf382826f984dbbd5d7c
20230720 <ghkdwo34@naver.com>
2024-01-25 09:01:39 +0900

    검색_기능3(서버연결수정)

--- src/ component/UI/Page/Conversion/Conversion_Separation/ConversionSearch.js
+++ src/ component/UI/Page/Conversion/Conversion_Separation/ConversionSearch.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
-import '././CoverdionSearch.scss'
+import '././CoverdionSearch.scss';
+import { SCORE_URL } from './server';
 import axios from 'axios';
 
 const ConversionSearch = () => {
@@ -11,14 +12,25 @@ const ConversionSearch = () => {
         try {
             if (!youtubeLink) return;
 
-            const backendUrl = 'http://localhost:8484/api/score/youtube';
+            // SCORE_URL을 사용하여 서버에 요청을 보내고 결과를 받아옴
+            // axios는 응답 데이터를 자동으로 JSON으로 파싱해주지만, fetch는 기본적으로 그렇지 않습니다.
+            // 따라서 fetch를 사용할 때는 수동으로 response.json()을 호출하여 JSON 데이터를 파싱
+            const response = await axios({
+                method: 'POST',
+                url: SCORE_URL,
+                data: { youtubeLink },
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
 
-            const response = await axios.post(backendUrl, { youtubeLink });
-
+            // 서버에서 받은 악보 정보를 상태에 업데이트
             setSheetMusic(response.data.sheetMusic);
         } catch (error) {
             alert(`검색 중 에러 발생: ${error.message}`);
         }
+
+        // 검색 후에는 유튜브 링크를 비움
         setYoutubeLink('');
     };
 
--- src/ component/UI/Page/Conversion/Conversion_Separation/server.js
+++ src/ component/UI/Page/Conversion/Conversion_Separation/server.js
@@ -1,6 +1,5 @@
 const LOCAL_PORT: string = '8484';
 const API_BASE_URL = 'http://localhost:';
-const SCORE : string = '/api/score';
-// const AUTH : string = '/api/auth';
-export const TODO_URL = API_BASE_URL + SCORE;
-// export const AUTH_URL = API_BASE_URL + AUTH;
+const SCORE: string = '/api/score/youtube ';
+export const SCORE_URL = API_BASE_URL + SCORE;
+// export const AUTH_URL = API_BASE_URL + '/api/auth'; // 이 부분은 아직 사용되지 않으므로 주석 처리
