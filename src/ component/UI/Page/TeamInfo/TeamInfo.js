f4541f2f858d1a53f24361c2f2b0346c43582c31
Won.Y.Jae <128454859+Y-ja@users.noreply.github.com>
2024-01-27 16:59:15 +0900

    Update TeamInfo.js
    팀페이지 수정2

--- src/ component/UI/Page/TeamInfo/TeamInfo.js
+++ src/ component/UI/Page/TeamInfo/TeamInfo.js
@@ -4,8 +4,7 @@ import React, { useState } from 'react';
 import r from './img/r.png';
 import b from './img/b.png';
 import p from './img/p.png';
-import Swiper from "swiper";
-import m12 from "../WebInfo/img/m12.png";
+
 
 const TeamInfo = ({ isForward }) => {
     const setAnimation = classNames({
@@ -20,18 +19,19 @@ const TeamInfo = ({ isForward }) => {
             </h1>
             <div className={`teaminfoContainer ${setAnimation}`}>
                 <h2>
-                   <p>팀 소개 페이지 입니다!</p>
-
-                    음악을 더 풍부하게 즐기기 위해 음원을 악보로 변환하는 고급 음악 변환 사이트를 개발하는 팀입니다.<br/>
+                    <p>팀 소개 페이지 입니다!</p>
                 </h2>
                 <div className="top">
                     <div className="box">
-                        <img src={r} className="img1" alt="instruction1" />
-                        <h1 className="heading">Y.JAE!</h1>
+                        <div className="box-tool">
+                            <img src={r} className="img1" alt="instruction1"/>
+                        </div>
+                        <h1 className="heading">REACT</h1>
                         <div className="data">
                             <span className="name">Won</span>
                         </div>
                         <p className="texts">
+                            원용재
                         </p>
                     </div>
                     <div className="box">
