50d7c2ac3f590c52451d56166391331fe382685f
Won.Y.Jae <128454859+Y-ja@users.noreply.github.com>
2024-01-28 14:54:46 +0900

    Update TeamInfo.js
    팀프로젝트 jax수정

--- src/ component/UI/Page/TeamInfo/TeamInfo.js
+++ src/ component/UI/Page/TeamInfo/TeamInfo.js
@@ -5,7 +5,6 @@ import r from './img/r.png';
 import b from './img/b.png';
 import p from './img/p.png';
 
-
 const TeamInfo = ({ isForward }) => {
     const setAnimation = classNames({
         'slide-up': isForward,
@@ -49,30 +48,39 @@ const TeamInfo = ({ isForward }) => {
                 </div>
                 <div className="bottom">
                     <div className="box">
-                        <img src={r} className="img1" alt="instruction1"/>
-                        <h1 className="heading">Y.JAE!</h1>
+                        <div className="box-tool">
+                            <img src={r} className="img1" alt="instruction1"/>
+                        </div>
+                        <h1 className="heading">REACT</h1>
                         <div className="data">
                             <span className="name">Won</span>
                         </div>
                         <p className="texts">
+                            원용재
                         </p>
                     </div>
                     <div className="box">
-                    <img src={r} className="img1" alt="instruction1" />
-                        <h1 className="heading">Y.JAE!</h1>
+                        <div className="box-tool">
+                            <img src={r} className="img1" alt="instruction1"/>
+                        </div>
+                        <h1 className="heading">REACT</h1>
                         <div className="data">
                             <span className="name">Won</span>
                         </div>
                         <p className="texts">
+                            원용재
                         </p>
                     </div>
                     <div className="box">
-                        <img src={r} className="img1" alt="instruction1" />
-                        <h1 className="heading">Y.JAE!</h1>
+                        <div className="box-tool">
+                            <img src={r} className="img1" alt="instruction1"/>
+                        </div>
+                        <h1 className="heading">REACT</h1>
                         <div className="data">
                             <span className="name">Won</span>
                         </div>
                         <p className="texts">
+                            원용재
                         </p>
                     </div>
                 </div>
