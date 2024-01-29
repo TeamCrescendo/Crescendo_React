b28edcc3660a2a05e5f10a3b96fbba15488f8f52
rudrbgus <rudrbgus3@gmail.com>
2024-01-29 23:26:29 +0900

    Board
    꾸미는중, 값 아직 안받아옴
    페이징해서 가져와야함

--- src/ component/UI/Page/Board/Board.js
+++ src/ component/UI/Page/Board/Board.js
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import './Board.scss';
 import classNames from "classnames";
-import ImageMasonry from "../../board/board_list/ImageMansonry";
-import PaginationRounded from "../../board/page_button/Page_Button";
 import {useState} from "react";
-import BoardDetail from "./board_detail/Board_Detail";
+import {ImageList, ImageListItem} from "@mui/material";
 
 const Board = ({ isForward }) => {
     const [scoreDetailOpen ,setScoreDetailOpen] = useState(false);
@@ -14,40 +12,103 @@ const Board = ({ isForward }) => {
         'slide-up': isForward,
         'slide-down': !isForward,
     });
-    const detailClose = () => {
-        setScoreDetailOpen(false);
-    }
+    
 
-    const renderBoardPage = (score_no) => {
-        // console.log(score_no);
-        setScoreDetailOpen(true);
-        setScoreId(score_no);
-    }
-    const renderBoardDetail = () => {
-        return (
-          <>
-            <BoardDetail score_no={scoreId} detailClose={detailClose}/>
-          </>
-        );
-    }
-    const renderBoardList = () => {
-        return (
-          <>
-              <ImageMasonry renderBoardPage={renderBoardPage}/>
-              {PaginationRounded()}
-          </>
-        );
-    }
+    const itemData = [
+        {
+            img: 'https://cdn.mapianist.com/preview-v2/0a0494b7-1db8-44b1-a0a1-175d076bd400-1675072813.jpg',
+            title: 'Fern',
+            score_no: 1,
+        },
+        {
+            img: 'https://cdn.mapianist.com/preview-v2/0a0494b7-1db8-44b1-a0a1-175d076bd400-1675072813.jpg',
+            title: 'Snacks',
+            score_no: 2,
+        },
+        {
+            img: 'https://cdn.mapianist.com/preview-v2/0a0494b7-1db8-44b1-a0a1-175d076bd400-1675072813.jpg',
+            title: 'Mushrooms',
+            score_no: 3,
+        },
+        {
+            img: 'https://cdn.mapianist.com/preview-v2/0a0494b7-1db8-44b1-a0a1-175d076bd400-1675072813.jpg',
+            title: 'Tower',
+        },
+        {
+            img: 'https://cdn.mapianist.com/preview-v2/0a0494b7-1db8-44b1-a0a1-175d076bd400-1675072813.jpg',
+            title: 'Sea star',
+        },
+        {
+            img: 'https://cdn.mapianist.com/preview-v2/0a0494b7-1db8-44b1-a0a1-175d076bd400-1675072813.jpg',
+            title: 'Honey',
+        },
+        {
+            img: 'https://cdn.mapianist.com/preview-v2/0a0494b7-1db8-44b1-a0a1-175d076bd400-1675072813.jpg',
+            title: 'Basketball',
+        },
+        {
+            img: 'https://cdn.mapianist.com/preview-v2/0a0494b7-1db8-44b1-a0a1-175d076bd400-1675072813.jpg',
+            title: 'Breakfast',
+        },
+        {
+            img: 'https://cdn.mapianist.com/preview-v2/0a0494b7-1db8-44b1-a0a1-175d076bd400-1675072813.jpg',
+            title: 'Tree',
+        },
+        {
+            img: 'https://cdn.mapianist.com/preview-v2/0a0494b7-1db8-44b1-a0a1-175d076bd400-1675072813.jpg',
+            title: 'Burger',
+        },
+        {
+            img: 'https://cdn.mapianist.com/preview-v2/0a0494b7-1db8-44b1-a0a1-175d076bd400-1675072813.jpg',
+            title: 'Camera',
+        },
+        {
+            img: 'https://cdn.mapianist.com/preview-v2/0a0494b7-1db8-44b1-a0a1-175d076bd400-1675072813.jpg',
+            title: 'Coffee',
+        },
+        {
+            img: 'https://cdn.mapianist.com/preview-v2/0a0494b7-1db8-44b1-a0a1-175d076bd400-1675072813.jpg',
+            title: 'Camping Car',
+        },
+        {
+            img: 'https://cdn.mapianist.com/preview-v2/0a0494b7-1db8-44b1-a0a1-175d076bd400-1675072813.jpg',
+            title: 'Hats',
+        },
+        {
+            img: 'https://cdn.mapianist.com/preview-v2/0a0494b7-1db8-44b1-a0a1-175d076bd400-1675072813.jpg',
+            title: 'Tomato basil',
+        },
+    ];
 
+    const detailHandler = (e) => {
+
+    }
 
     return (
         <div className={`boardContainer ${setAnimation}`}>
-            {
-                scoreDetailOpen
-                ? renderBoardDetail()
-                : renderBoardList()
-            }
-
+            <ImageList sx={{width:800, height:600}} cols = {3} rowHeight={400}>
+                {itemData.map((item) => (
+                    <ImageListItem key={item.img} className="image-list-item" sx={{
+                        border:1,
+                        boardColor: 'primary.main',
+                        boardRadius: 12,
+                        gap: 1
+                    }}>
+                        <img
+                            className="score-img"
+                            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                            alt={item.title}
+                            loading="lazy"
+                        />
+                        <div className="image-text" onClick={detailHandler}  id={item.score_no}>
+                            곡명
+                            <span className="score-title">{item.title}</span>
+                            <div className="score-info"><span>자세히 보기</span></div>
+                        </div>
+                    </ImageListItem>
+                ))}
+            </ImageList>
         </div>
     );
 };
