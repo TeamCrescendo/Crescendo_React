ebe0b355ca807f2f6d8c65b01537cd94d1bd1086
Won.Y.Jae <128454859+Y-ja@users.noreply.github.com>
2024-01-31 21:51:59 +0900

    Update MyPage.js
    마페 폼 만들기

--- src/ component/UI/Page/MyPage/MyPage.js
+++ src/ component/UI/Page/MyPage/MyPage.js
@@ -1,6 +1,7 @@
-import React, { useState, useRef } from 'react';
+import React, {useState, useEffect} from 'react';
 import './MyPage.scss';
 import classNames from "classnames";
+import { SlClose } from "react-icons/sl"
 
 const MyPage = ({ isForward }) => {
     const setAnimation = classNames({
@@ -15,8 +16,35 @@ const MyPage = ({ isForward }) => {
                     My Page
                 </h1>
                 <div className={`webinfoContainer ${setAnimation}`}>
-                    <h2>
-                    </h2>
+                    <div className="card">
+                        <div className="card-img">
+                            <div className="card-head">
+                                <span className="close-logo"><SlClose/></span>
+                                <h4>
+                                    Alarm
+                                </h4>
+                                <h3 className="card-title">
+                                    앗 잠시만요!
+                                </h3>
+
+                            </div>
+                        </div>
+                        <div className="card-main">
+                            <br/>
+                            <p className="card-text">
+                                마이페이지를 볼려면 로그인을 하세요!
+                            </p>
+                        </div>
+                        <div class="card-footer">
+                            <a href="#" className="btn btn-primary" >
+                                LOGIN
+                            </a>
+                            <br/>
+                            <a href="#">
+                                <span className="join-text">로그인이 없으신가요?</span>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </header>
         </>
