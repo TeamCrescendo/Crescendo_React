77a2b818f1eb6618a0fac165f854053ff6553911
20230720 <ghkdwo34@naver.com>
2024-01-31 17:49:51 +0900

    폼_만들기2(수정)

--- src/ component/UI/Page/Board/Board.js
+++ src/ component/UI/Page/Board/Board.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import './Board.scss';
 import classNames from "classnames";
-import { IoMdCloseCircle } from "react-icons/io";
-import r from "../TeamInfo/img/r.png";
-
+import axios from 'axios';
+import { FaFileUpload } from "react-icons/fa";
+import { IoCloseCircleOutline } from "react-icons/io5";
 const Board = ({ isForward }) => {
     const setAnimation = classNames({
         'slide-up': isForward,
@@ -13,7 +13,26 @@ const Board = ({ isForward }) => {
     const handleContentChange = (e) => {
         setContent(e.target.value); // textarea의 값이 변경될 때마다 state 업데이트
     };
+    // PDF 파일 업로드 관련 함수
+    // PDF 파일 업로드 관련 함수
+    const handlePdfUpload = async (event) => {
+        const pdfFile = event.target.files[0];
 
+        try {
+            const formData = new FormData();
+            formData.append('pdfFile', pdfFile);
+
+            const uploadResponse = await axios.post('/api/upload/pdf', formData); // axios 사용
+            console.log('======= PDF 파일 업로드 성공 =======');
+            console.log(uploadResponse.data);
+
+            // 여기에서 필요한 처리를 추가할 수 있습니다.
+
+        } catch (uploadError) {
+            console.log('======= PDF 파일 업로드 실패 =======');
+            console.log(uploadError);
+        }
+    };
     return (
         <>
             <header className="hhh1">
@@ -22,13 +41,14 @@ const Board = ({ isForward }) => {
                 </h1>
                 <div className={`boardContainer ${setAnimation}`}>
                     <div className="box2">
+                        <span className="imog2"><IoCloseCircleOutline /></span>
                         <p className="texts2">
                             자유롭게 적어 주세요!
+
                         </p>
+
                         <div className="boot-strap2">
                             <hr/>
-                        </div>
-                        <div className="title2">
                             <input
                                 type="text"
                                 className="content2"
@@ -37,9 +57,19 @@ const Board = ({ isForward }) => {
                                 onChange={handleContentChange}
                             />
                         </div>
-                        <div className="heading2">
-                            upload
+                        <div className="title2">
                         </div>
+                        <div className="heading2">
+                            <input
+                                className="upload2"
+                                type="file"
+                                onChange={handlePdfUpload}
+                                accept=".pdf"
+                            />
+                            <p><FaFileUpload /> 파일을 업로드 하세요!</p>
+                            <button className="button2">UPLOAD</button>
+                        </div>
+
                     </div>
                 </div>
             </header>
