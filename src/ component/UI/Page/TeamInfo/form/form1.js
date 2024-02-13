c162f89729a3a6a0bcf4dfbf2d3f8ead8032101e
20230720 <ghkdwo34@naver.com>
2024-02-13 16:10:07 +0900

    팀페이지 디자인 수정

--- src/ component/UI/Page/TeamInfo/form/form1.js
+++ src/ component/UI/Page/TeamInfo/form/form1.js
@@ -11,13 +11,13 @@ const Form1 = () => {
                 <span className="span-title1">
                     " Unexpressed emotions through sound, that's what music is! "
                 </span><br/>
-                <p>
+                <p className="p-title1">
                     사용자가 음원 파일을 업로드하면, 빠르고 정확한 변환 알고리즘이 적용되어 즉시 악보로 변환됩니다.<br/>
                     도구를 사용하여 음악 파일을 악보로 변환하고, 빠르게 음악을 이해하며 연주하는 새로운 경험을 즐겨보세요!
                 </p>
             </div>
             <div className="title3">
-                <h2 className="h3-title">팀소개 입니다!</h2>
+                <h2 className="h3-title">TEAM</h2>
             </div>
             <div className="top">
                 <div className="box">
