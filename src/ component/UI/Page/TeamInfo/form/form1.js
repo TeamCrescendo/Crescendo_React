7202424cd55950112c549f08f91cb4bc635b2433
ghkdw <ghkdwo34@naver.com>
2024-02-13 22:07:25 +0900

    팀플 페이지 수정

--- src/ component/UI/Page/TeamInfo/form/form1.js
+++ src/ component/UI/Page/TeamInfo/form/form1.js
@@ -6,17 +6,16 @@ import './Form.scss'
 const Form1 = () => {
     return (
         <>
-            <div className="title1">
-                <h1 className="h1-title">음악 파일을 악보로 바로 변환, 한번에 즐기기</h1>
-                <span className="span-title1">
+                <div className="title1">
+                    <h1 className="h1-title">음악 파일을 악보로 바로 변환, 한번에 즐기기</h1>
+                    <span className="span-title1">
                     " Unexpressed emotions through sound, that's what music is! "
-                </span><br/>
-                <p className="p-title1">
-                    사용자가 음원 파일을 업로드하면, 빠르고 정확한 변환 알고리즘이 적용되어 즉시 악보로 변환됩니다.<br/>
-                    도구를 사용하여 음악 파일을 악보로 변환하고, 빠르게 음악을 이해하며 연주하는 새로운 경험을 즐겨보세요!
-                </p>
-            </div>
-
+                    </span><br/>
+                    <p className="p-title1">
+                        사용자가 음원 파일을 업로드하면, 빠르고 정확한 변환 알고리즘이 적용되어 즉시 악보로 변환됩니다.<br/>
+                        도구를 사용하여 음악 파일을 악보로 변환하고, 빠르게 음악을 이해하며 연주하는 새로운 경험을 즐겨보세요!
+                    </p>
+                </div>
             <div className="title3">
                 <h2 className="h3-title">TEAM</h2>
             </div>
